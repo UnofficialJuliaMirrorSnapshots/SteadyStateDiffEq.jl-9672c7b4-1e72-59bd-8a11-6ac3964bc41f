194d14442f77746fc4ab8736bc7853f423d04f2b
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-06-30 05:34:22 +0000

    Snapshot of branch master taken on 2019-06-30T01:34:21.088-04:00 from "https://github.com/UnofficialJuliaMirror/SteadyStateDiffEq.jl-9672c7b4-1e72-59bd-8a11-6ac3964bc41f"

--- src/solve.jl
+++ src/solve.jl
@@ -41,14 +41,27 @@ function DiffEqBase.__solve(prob::DiffEqBase.AbstractSteadyStateProblem,
 end
 
 function DiffEqBase.__solve(prob::DiffEqBase.AbstractSteadyStateProblem,
-                            alg::DynamicSS,args...;kwargs...)
+                            alg::DynamicSS,args...;save_everystep=false,
+                            save_start=false,kwargs...)
 
   tspan = alg.tspan isa Tuple ? alg.tspan : (zero(alg.tspan), alg.tspan)
   _prob = ODEProblem(prob.f,prob.u0,tspan,prob.p)
   sol = solve(_prob,alg.alg,args...;kwargs...,
-              callback=TerminateSteadyState(alg.abstol,alg.reltol))
+              callback=TerminateSteadyState(alg.abstol,alg.reltol),
+              save_everystep=save_everystep,save_start=save_start)
   if sol.t[end] == _prob.tspan[end]
     sol = DiffEqBase.solution_new_retcode(sol, :Failure)
+  elseif sol.retcode == :Terminated
+    if isinplace(prob)
+      du = similar(sol.u[end])
+      prob.f(du, sol.u[end], prob.p, sol.t[end])
+    else
+      du = prob.f(sol.u[end], prob.p, sol.t[end])
+    end
+    if all(abs(d) <= abstol || abs(d) <= reltol*abs(u) for (d,abstol, reltol, u) =
+      zip(du, Iterators.cycle(alg.abstol), Iterators.cycle(alg.reltol), sol.u[end]))
+      sol = DiffEqBase.solution_new_retcode(sol, :Success)
+    end
   end
   sol
 end
